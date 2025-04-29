2c229dab9e23fff50ba7b0dd7ddd62af86505a42
Jamal Henderson <jamtmac1@gmail.com>
2025-04-28 23:06:59 -0400

    added check to force the use of special characters and numbers

--- password-checker.py
+++ password-checker.py
@@ -2,6 +2,9 @@
 # Ask user to input a password
  
 password = input("Enter your password: ")
+# Check for numbers
+has_number = any(char.isdigit() for char in password)
+has_special = any(not char.isalnum() for char in password)
 
 # Strength options 
 if len(password) <8:
@@ -9,4 +12,9 @@ if len(password) <8:
 elif len(password) <=12:
 	print("Password is moderate. Please make it stronger")
 else: 
-	print("Password is strong")
+	if has_number and has_special:
+		print("Password is strong")
+	else:
+		print("Password must contain a numeric value and special character")
+
+
