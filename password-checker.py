686e5db58aa595f814c27274a159b09396b0e0a3
Jamal Henderson <jamtmac1@gmail.com>
2025-04-29 23:48:01 -0400

    Enhanced payment to get around trust now.

--- password-checker.py
+++ password-checker.py
@@ -1,20 +1,23 @@
 
 # Ask user to input a password
- 
-password = input("Enter your password: ")
-# Check for numbers
-has_number = any(char.isdigit() for char in password)
-has_special = any(not char.isalnum() for char in password)
 
-# Strength options 
-if len(password) <8:
-	print("Password is too weak")
-elif len(password) <=12:
-	print("Password is moderate. Please make it stronger")
-else: 
-	if has_number and has_special:
-		print("Password is strong")
-	else:
-		print("Password must contain a numeric value and special character")
+while True:
+	password = input("Enter your password: ")
+
+	# Strength options 
+	if len(password) <8:
+		print("Password is too weak")
+	elif len(password) <=12:
+		print("Password is moderate. Please make it stronger")
+	else: 
+		has_number = any(char.isdigit() for char in password)
+		has_special = any(not char.isalnum() for char in password)
+
+		if has_number and has_special:
+			print("Password is strong")
+			break #Learning that break is not the ideal way to exit a loop but okay sense this is a tiny loop
+		else:
+			print("Password must contain a numeric value and special character")
+		
 
 
