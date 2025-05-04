75420959966acd6f8cb9911b86c4f33f8e703c61
Jamal Henderson <jamtmac1@gmail.com>
2025-05-03 22:59:06 -0400

    Switched up to make user flow is goood

--- password-checker.py
+++ password-checker.py
@@ -2,15 +2,11 @@
 import re
 # Placeholder to refresh on regex since I saw that regex would be a better usecase here for more complex loops
 
-#
-attempts = 0
-max_attempts = 5
-
 # Starts Loop
 while True:
 	# Ask user to input password
 	password = input("Enter your password: ")
-	attempts += 1
+
 	# Strength options 
 	if len(password) <8:
 		print("Password is too weak")
@@ -22,11 +18,17 @@ while True:
 
 		if has_number and has_special: 
 			print("Password is strong") 
-			print(f"Password accepted after {attempts} attempts.")
 			break #Learning that break is not the ideal way to exit a loop but okay sense this is 
 			a tiny loop
 		else:
 			print("Password must contain a numeric value and special character")
+
+# Account creation
+
+while True:
+	new_password = input("Create password: ")
+	confirm_password = input("Confirm new password: ")
+	if new_password !=  confirm_password
 
 			
 
