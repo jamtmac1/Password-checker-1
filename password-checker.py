ed2306053fb4c9769e29bac6243dc803973c66c8
Jamal Henderson <jamtmac1@gmail.com>
2025-05-06 22:42:17 -0400

    Added more logic

--- password-checker.py
+++ password-checker.py
@@ -5,34 +5,57 @@ import re
 # Starts Loop
 while True:
 	# Ask user to input password
-	password = input("Enter your password: ")
+	password = input("Create a password: ")
+	confirm_password = input("Confirm your password: ")
+
+	# Passwords Match?
+	if password!= confirm_password:
+		print("Passwords do not match")
 
 	# Strength options 
 	if len(password) <8:
 		print("Password is too weak")
-	elif len(password) <=12:
+	elif len(password) <=11:
 		print("Password is moderate. Please make it stronger")
 	else: 
 		has_number = any(char.isdigit() for char in password)
 		has_special = any(not char.isalnum() for char in password)
 
 		if has_number and has_special: 
-			print("Password is strong") 
-			break #Learning that break is not the ideal way to exit a loop but okay sense this is 
-			a tiny loop
+			print("Password is strong and accepted") 
+			break #Learning that break is not the ideal way to exit a loop but okay since this is a tiny loop
 		else:
 			print("Password must contain a numeric value and special character")
-
-# Account creation
-
+			continue
+# Password Hint
 while True:
-	new_password = input("Create password: ")
-	confirm_password = input("Confirm new password: ")
-	if new_password !=  confirm_password
-		print ("Passwords do not match")
-		continue
+	password_hint = input("Create a password hint: ")
+	print("Your password hint has been saved.")
+	break
 
+# Account Creation
+max_attempts = 5
+show_hint = False
+login_attempts = 0
 
-			
+print("Login")
 
-
+while login_attempts < max_attempts:
+	entered_password = input("Enter your password: ")
+	login_attempts += 1
+	
+	if entered_password == password:
+		print("Password Accepted")
+		break
+	else:
+		print("Incorrect Password: Try Again.")
+		
+		if login_attempts == 3 and not show_hint:
+			print(f"Hint: {password_hint}")
+			show_hint = True
+		
+		attempts_left = max_attempts - login_attempts
+		if attempts_left > 0:
+			print(f"You have {attempts_left} attempts left.")
+if login_attempts == max_attempts:
+	print("Too many failed attempts. You have been locked out of the account. Please contact an administrator")
